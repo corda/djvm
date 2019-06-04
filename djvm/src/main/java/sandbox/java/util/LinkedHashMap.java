a5e342a1c3b28d7beb295ac554736d0149da43be
Chris Rankin <chris.rankin@r3.com>
2019-06-04 09:29:37 +0100

    Simplify bootstrapping of sandbox.java.util.LinkedHashMap class.

--- djvm/src/main/java/sandbox/java/util/LinkedHashMap.java
+++ djvm/src/main/java/sandbox/java/util/LinkedHashMap.java
@@ -1,13 +1,27 @@
 package sandbox.java.util;
 
 /**
- * This is a dummy class to bootstrap us into the sandbox.
+ * This is a dummy class that implements just enough of {@link java.util.LinkedHashMap}
+ * to allow us to compile {@link sandbox.java.lang.DJVM}.
  */
-public class LinkedHashMap<K, V> extends java.util.LinkedHashMap<K, V> implements Map<K, V> {
+public class LinkedHashMap<K, V> extends sandbox.java.lang.Object implements Map<K, V> {
+    private final java.util.LinkedHashMap<K, V> map;
+
     public LinkedHashMap(int initialSize) {
-        super(initialSize);
+        map = new java.util.LinkedHashMap<>(initialSize);
     }
 
     public LinkedHashMap() {
+        map = new java.util.LinkedHashMap<>();
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return map.put(key, value);
+    }
+
+    @Override
+    public V get(K key) {
+        return map.get(key);
     }
 }
