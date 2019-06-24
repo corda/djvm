95c491535001422cf1c48a25772e26889de50723
Chris Rankin <chris.rankin@r3.com>
2019-06-25 00:09:52 +0100

    Fix JavaDoc for DJVMException interface.

--- djvm/src/main/java/sandbox/java/lang/DJVMException.java
+++ djvm/src/main/java/sandbox/java/lang/DJVMException.java
@@ -1,12 +1,12 @@
 package sandbox.java.lang;
 
 /**
- * All synthetic [Throwable] classes wrapping non-JVM exceptions
+ * All synthetic {@link java.lang.Throwable} classes wrapping non-JVM exceptions
  * will implement this interface.
  */
 public interface DJVMException {
     /**
-     * Returns the [sandbox.java.lang.Throwable] instance inside the wrapper.
+     * Returns the {@link sandbox.java.lang.Throwable} instance inside the wrapper.
      */
     Throwable getThrowable();
 }
