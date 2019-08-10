0b0ff6ad25b077f3b7916dbc97855faa7bb51c03
Chris Rankin <chris.rankin@r3.com>
2019-08-10 14:07:13 +0100

    CORDA-2871: Resolve warnings for dummy Exception classes. (#43)

--- djvm/src/main/java/sandbox/java/util/MissingResourceException.java
+++ djvm/src/main/java/sandbox/java/util/MissingResourceException.java
@@ -1,13 +1,14 @@
 package sandbox.java.util;
 
 import sandbox.java.lang.String;
+import sandbox.java.lang.Throwable;
 
 /**
  * This is a dummy class that implements just enough of {@link java.util.MissingResourceException}
  * to allow us to compile {@link sandbox.java.lang.DJVM}.
  */
 @SuppressWarnings("unused")
-public class MissingResourceException extends sandbox.java.lang.Throwable {
+public class MissingResourceException extends Throwable {
     public MissingResourceException(String baseName, String className, String key) {
     }
 }
