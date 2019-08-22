fccb7669b9c4304174f54e965ad4e968bdfb06d3
Chris Rankin <chris.rankin@r3.com>
2019-08-22 16:43:00 +0100

    CORDA-3154: Map the kotlin.Metadata.d2[] strings into the sandbox package space. (#49)
    * Map the kotlin.Metadata.d2[] strings into the sandbox package space.
    * Add KDoc comment for KotlinMetadataVisitor.
    * Deactivate DEBUG logging by default for tests.

---
+++ djvm/src/main/kotlin/net/corda/djvm/rewiring/KotlinMetadataVisitor.kt
@@ -0,0 +1,43 @@
+package net.corda.djvm.rewiring
+
+import org.objectweb.asm.AnnotationVisitor
+import org.objectweb.asm.commons.Remapper
+
+/**
+ * Maps the descriptor strings inside [Metadata.d2] into the sandbox.*
+ * package space. This means that Kotlin reflection will now generate
+ * the new sandboxed method and field signatures when it "rehydrates"
+ * the ProtoBuf data inside the [Metadata.d1] field.
+ *
+ * From an end-user perspective, this realigns Kotlin reflection with
+ * Java reflection and allows Kotlin reflection to (e.g.) identify and
+ * invoke the new class's primary constructor.
+ */
+class KotlinMetadataVisitor(
+    api: Int,
+    av: AnnotationVisitor,
+    private val remapper: Remapper
+) : AnnotationVisitor(api, av) {
+    companion object {
+        val INTERNAL_NAME = "L([^.;\\[]++)+;".toRegex()
+    }
+
+    override fun visitArray(name: String): AnnotationVisitor? {
+        return super.visitArray(name)?.let {
+            if (name == "d2") KotlinMetadataTransformer(it) else it
+        }
+    }
+
+    private inner class KotlinMetadataTransformer(av: AnnotationVisitor) : AnnotationVisitor(api, av) {
+        override fun visit(name: String?, value: Any?) {
+            val newValue = if (value !is String || !INTERNAL_NAME.containsMatchIn(value)) {
+                value
+            } else if (value.startsWith('(')) {
+                remapper.mapMethodDesc(value)
+            } else {
+                remapper.mapDesc(value)
+            }
+            super.visit(name, newValue)
+        }
+    }
+}
