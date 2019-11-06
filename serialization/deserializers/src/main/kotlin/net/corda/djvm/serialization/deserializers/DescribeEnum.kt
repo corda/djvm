71d9766fa77d899201a60beef25219426b898b78
Chris Rankin <chris.rankin@r3.com>
2019-11-06 15:38:56 +0000

    CORDA-2876: Remove DJVM serialization modules, which are now part of Corda. (#104)

--- serialization/deserializers/src/main/kotlin/net/corda/djvm/serialization/deserializers/DescribeEnum.kt
+++
@@ -1,9 +0,0 @@
-package net.corda.djvm.serialization.deserializers
-
-import java.util.function.Function
-
-class DescribeEnum : Function<Class<*>, Array<out Any?>> {
-    override fun apply(enumClass: Class<*>): Array<out Any?> {
-        return enumClass.enumConstants
-    }
-}
