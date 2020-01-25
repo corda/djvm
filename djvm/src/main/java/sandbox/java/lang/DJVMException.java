757cc74e527e7694257a75ef74e9395565577ea3
Chris Rankin <chris.rankin@r3.com>
2020-01-25 19:00:11 +0000

    CORDA-3377: Annotate interfaces as @FunctionalInterface.

--- djvm/src/main/java/sandbox/java/lang/DJVMException.java
+++ djvm/src/main/java/sandbox/java/lang/DJVMException.java
@@ -4,6 +4,7 @@ package sandbox.java.lang;
  * All synthetic {@link java.lang.Throwable} classes wrapping non-JVM exceptions
  * will implement this interface.
  */
+@FunctionalInterface
 public interface DJVMException {
     /**
      * Returns the {@link sandbox.java.lang.Throwable} instance inside the wrapper.
--- djvm/src/test/kotlin/foo/bar/sandbox/Callable.kt
+++ djvm/src/test/kotlin/foo/bar/sandbox/Callable.kt
@@ -3,6 +3,7 @@ package foo.bar.sandbox
 /**
  * Interface used for implementing test classes.
  */
+@FunctionalInterface
 interface Callable {
 
     /**
