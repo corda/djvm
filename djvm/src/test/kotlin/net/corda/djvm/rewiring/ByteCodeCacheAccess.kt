bf3c223ed28546630aca3f77fc2a8cb2dec142c1
Chris Rankin <chris.rankin@r3.com>
2020-01-31 10:59:58 +0000

    CORDA-3377: Extend test coverage for synthetic exception classes.

--- djvm/src/test/kotlin/net/corda/djvm/rewiring/ByteCodeCacheAccess.kt
+++ djvm/src/test/kotlin/net/corda/djvm/rewiring/ByteCodeCacheAccess.kt
@@ -5,3 +5,11 @@ package net.corda.djvm.rewiring
  * Expose this function for testing only.
  */
 fun ByteCodeCache.flush() = clear()
+
+fun ByteCodeCache.flushAll() {
+    var cache = this
+    do {
+        cache.flush()
+        cache = cache.parent ?: break
+    } while (true)
+}
