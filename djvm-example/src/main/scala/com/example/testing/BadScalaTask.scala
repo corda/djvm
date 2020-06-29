186849f34a7ba33e999f72247668f8af6f4a7051
Chris Rankin <chris.rankin@r3.com>
2020-06-29 09:39:14 +0100

    CORDA-3870: Upgrade libraries for 1.2 (#170)
    Upgrade the following libraries for the DJVM:
    - ASM 7.3.1 → 8.0.1
    - Log4J2 2.13.1 → 2.13.3
    - AssertJ 3.15.0 → 3.16.1
    - ClassGraph 4.8.65 → 4.8.78
    
    Example project:
    - Corda 4.5-SNAPSHOT → 4.6-SNAPSHOT
    - Kotlin 1.3.70 → 1.3.72
    - Scala 2.13.1 → 2.13.3
    - JUnit Jupiter 5.6.0 → 5.6.2

--- djvm-example/src/main/scala/com/example/testing/BadScalaTask.scala
+++ djvm-example/src/main/scala/com/example/testing/BadScalaTask.scala
@@ -1,13 +1,7 @@
 package com.example.testing
 
-import java.security.MessageDigest
 import java.util.function.Function
 
-final class BadScalaTask extends Function[Array[Byte], String] {
-    override def apply(input: Array[Byte]): String = {
-        MessageDigest.getInstance("SHA-256")
-            .digest(input)
-            .map("%02x" format _)
-            .mkString
-    }
+final class BadScalaTask extends Function[String, Long] {
+    override def apply(input: String): Long = getClass().getField(input).getLong(this)
 }
